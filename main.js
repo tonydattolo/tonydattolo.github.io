3a9dbb3c97fc97f71d3bfaae0e7f2250f559bdd7
tonydattolo <tdattolo@iu.edu>
2020-10-12 02:05:10 -0400

    Testing Tables on Skills Page

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // // sticky nav menu
 // $(document).ready(function (){
 
-
 //     let nav_offset_top = $('.header_area').height() + 50;
 
 //     function navbarFixed() {
@@ -24,13 +23,25 @@
 // var items = document.getElementsByClassName("nav-item");
 // var links = header.getElementsByClassName("nav-link");
 
-var navCont = document.getElementById("navbarNav");
-var links = navCont.getElementsByClassName("nav-item");
+// var navCont = document.getElementById("navbarNav");
+// var links = navCont.getElementsByClassName("nav-item");
 
-for (let i = 0; i < links.length; i++) {
-    links[i].addEventListener("click", function () {
-        var currentActive = document.getElementsByClassName("active");
-        currentActive[0].classList.remove("active");
-        this.classList.add("active");
-    });
-}
+// for (let i = 0; i < links.length; i++) {
+//     links[i].addEventListener("click", function () {
+//         var currentActive = document.getElementsByClassName("active");
+//         currentActive[0].classList.remove("active");
+//         this.classList.add("active");
+//     });
+// }
+
+// $(document).ready(function() {
+//     $('navbar-nav li a').click(function() {
+//         $(this).siblings('li').removeClass('active');
+//         $(this).parent().addClass('active');
+//     });
+// });
+
+$(document).on("click", "ul li", function () {
+  $("li").removeClass("active");
+  $(this).addClass("active");
+});
